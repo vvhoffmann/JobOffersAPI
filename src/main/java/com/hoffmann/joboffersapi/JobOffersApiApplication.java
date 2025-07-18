380a1c9e71536a8ac6dfa0224d252d2ae6c7a0c6
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-18 12:51:20 +0200

    Initial commit

---
+++ src/main/java/com/hoffmann/joboffersapi/JobOffersApiApplication.java
@@ -0,0 +1,13 @@
+package com.hoffmann.joboffersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOffersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoffmann/joboffersapi/JobOffersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoffmann.joboffersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOffersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
