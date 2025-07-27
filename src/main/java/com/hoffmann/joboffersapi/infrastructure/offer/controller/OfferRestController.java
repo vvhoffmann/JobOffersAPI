9a0566aaacab3d028b79e31f1a5d17093cf0d495
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-27 12:24:25 +0200

    fixed offer rest controller bug

--- src/main/java/com/hoffmann/joboffersapi/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/hoffmann/joboffersapi/infrastructure/offer/controller/OfferRestController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +21,11 @@ class OfferRestController {
     public ResponseEntity<List<OfferResponseDto>> getAllOffers() {
         final List<OfferResponseDto> allOffers = offersFacade.findAllOffers();
         return new ResponseEntity<>(allOffers, HttpStatus.OK);
+    }
+
+    @GetMapping("/offers/{offerId}")
+    public ResponseEntity<OfferResponseDto> findOfferById(@RequestParam String offerId) {
+        final OfferResponseDto offerById = offersFacade.findOfferById(offerId);
+        return new ResponseEntity<>(offerById, HttpStatus.OK);
     }
 }
